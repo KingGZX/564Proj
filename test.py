2d07f7e4b4610bb61cd9d90cd77d68f81b7c1fbd
guzhexuan <2974127659@qq.com>
2023-02-23 21:11:58 +0800

    random test file

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 from helper_s3dis import Data_S3DIS
 import glob
+from model import Backbone_BoNet
 
 a = 'Area_1'
 con = glob.glob("./data/Data_S3DIS/" + a + '*.h5')
 
-data = Data_S3DIS("./data/Data_S3DIS/", ['Area_1', 'Area_2', 'Area_3', 'Area_4', 'Area_6'], ['Area_5'])
+data = Data_S3DIS("./data/Data_S3DIS/", ['Area_1'], ['Area_5'])
 
-data.load_train_next_batch()
+X, sem_labels, ins_labels, psem_labels, bb_labels, pmask_labels = data.load_train_next_batch()
+print(X.shape)
+
+net = Backbone_BoNet(9)
+import torch
+gf, pf = net(torch.tensor(X[:, :, :9]))
+
+print(gf.shape, pf.shape)
