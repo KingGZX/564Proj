e854450d922a457fcabf08cdaa9ae35b95d5a695
guzhexuan <2974127659@qq.com>
2023-04-12 17:25:03 +0800

    latest version of visualization test set

--- test.py
+++ test.py
@@ -15,9 +15,10 @@ test the procedures step by step
 
 """
 a = 'Area_1'
-con = glob.glob("./data/Data_S3DIS/" + a + '*.h5')
+con = glob.glob("./data/" + a + '*.h5')
 """
 
+"""
 data = Data_S3DIS("./data/", ['Area_1', 'Area_2', 'Area_3', 'Area_4', 'Area_6'], ['Area_5'])
 
 from train import train_transformer
@@ -25,7 +26,7 @@ from train import train_transformer
 net = Transformer(9, 4, 64)
 net2 = Semantic_Net()
 train_transformer([net, net2], data)
-
+"""
 
 
 """
@@ -68,32 +69,44 @@ print(C_ed.shape)
 C_sIoU = box_s_iou_cost(torch.tensor(X), torch.tensor(bb_labels), box)
 print(C_sIoU.shape)
 """
-"""
-fin = h5py.File("./Area_1_office_1.h5", 'r')
 
-coords = fin['coords'][:].reshape(-1, 3)
-points = fin['points'][:].reshape(-1, 9)
-pc_xyzrgb = np.concatenate([coords, points[:, 3:9]], axis=-1)
-min_x = np.min(pc_xyzrgb[:, 0])
-max_x = np.max(pc_xyzrgb[:, 0])
-min_y = np.min(pc_xyzrgb[:, 1])
-max_y = np.max(pc_xyzrgb[:, 1])
-min_z = np.min(pc_xyzrgb[:, 2])
-max_z = np.max(pc_xyzrgb[:, 2])
 
-ori_xyz = copy.deepcopy(pc_xyzrgb[:, 0:3])  # reserved for final visualization
-pc_xyzrgb[:, 0:1] = (pc_xyzrgb[:, 0:1] - min_x) / np.maximum((max_x - min_x), 1e-3)
-pc_xyzrgb[:, 1:2] = (pc_xyzrgb[:, 1:2] - min_y) / np.maximum((max_y - min_y), 1e-3)
-pc_xyzrgb[:, 2:3] = (pc_xyzrgb[:, 2:3] - min_z) / np.maximum((max_z - min_z), 1e-3)
-pc_xyzrgb = np.concatenate([pc_xyzrgb, ori_xyz], axis=-1)
+fin = h5py.File("./data/Area_5_hallway_1.h5", 'r')
+
+## (55, 4096, 3)  &    [55, 4096, 9]
+coords = fin['coords'][:]
+points = fin['points'][:]
+
+batch, num, features1 = coords.shape
+_, _, features2 = points.shape
+
+temp = np.zeros((batch, num, features1 + features2))
+
+for i in range(batch):
+    pc_xyzrgb = np.concatenate([coords[i], points[i][:, 3:9]], axis=-1)
+    min_x = np.min(pc_xyzrgb[:, 0])
+    max_x = np.max(pc_xyzrgb[:, 0])
+    min_y = np.min(pc_xyzrgb[:, 1])
+    max_y = np.max(pc_xyzrgb[:, 1])
+    min_z = np.min(pc_xyzrgb[:, 2])
+    max_z = np.max(pc_xyzrgb[:, 2])
+
+    ori_xyz = copy.deepcopy(pc_xyzrgb[:, 0:3])  # reserved for final visualization
+    pc_xyzrgb[:, 0:1] = (pc_xyzrgb[:, 0:1] - min_x) / np.maximum((max_x - min_x), 1e-3)
+    pc_xyzrgb[:, 1:2] = (pc_xyzrgb[:, 1:2] - min_y) / np.maximum((max_y - min_y), 1e-3)
+    pc_xyzrgb[:, 2:3] = (pc_xyzrgb[:, 2:3] - min_z) / np.maximum((max_z - min_z), 1e-3)
+    pc_xyzrgb = np.concatenate([pc_xyzrgb, ori_xyz], axis=-1)   # [points, 12]
+    temp[i] = pc_xyzrgb
+
+X = temp.reshape((-1, features1 + features2))
 # print(pc_xyzrgb.shape)
 
 
-backbone = torch.load("./backbone.pth", map_location=torch.device('cpu'))
+backbone = torch.load("J:\\COMProfile\\Downloads\\pk\\42\\backbone_42.pth", map_location=torch.device('cpu'))
 backbone.eval()
-seg_net = torch.load("./seg_net.pth", map_location=torch.device('cpu'))
+seg_net = torch.load("J:\\COMProfile\\Downloads\\pk\\42\\seg_net_42.pth", map_location=torch.device('cpu'))
 seg_net.eval()
-in_X = torch.tensor(pc_xyzrgb[None, :, :9]).clone().detach()
+in_X = torch.tensor(X[None, :, :9], dtype=torch.float32).clone().detach()
 f1, f2 = backbone(in_X)
 f1 = f1[None, :]
 # f2 = f2[None, :, :]
@@ -105,4 +118,105 @@ predict_sem_labels = predict_sem_labels.numpy()
 print(predict_sem_labels.shape)
 
 Plot.draw_pc_semins(fin['coords'][:].reshape(-1, 3), predict_sem_labels, 13)
+
+
+
+"""
+# test_transformer
+fin = h5py.File("./data/Area_5_WC_1.h5", 'r')
+
+# [55, 4096, 3]   &    [55, 4096, 9]
+coords = fin['coords'][:]
+points = fin['points'][:]
+
+batch, num, features1 = coords.shape
+
+_, _, features2 = points.shape
+
+temp = np.zeros((batch, num, features1 + features2), dtype=np.float32)
+
+for i in range(batch):
+    pc_xyzrgb = np.concatenate([coords[i], points[i][:, 3:9]], axis=-1)
+    min_x = np.min(pc_xyzrgb[:, 0])
+    max_x = np.max(pc_xyzrgb[:, 0])
+    min_y = np.min(pc_xyzrgb[:, 1])
+    max_y = np.max(pc_xyzrgb[:, 1])
+    min_z = np.min(pc_xyzrgb[:, 2])
+    max_z = np.max(pc_xyzrgb[:, 2])
+
+    ori_xyz = copy.deepcopy(pc_xyzrgb[:, 0:3])  # reserved for final visualization
+    pc_xyzrgb[:, 0:1] = (pc_xyzrgb[:, 0:1] - min_x) / np.maximum((max_x - min_x), 1e-3)
+    pc_xyzrgb[:, 1:2] = (pc_xyzrgb[:, 1:2] - min_y) / np.maximum((max_y - min_y), 1e-3)
+    pc_xyzrgb[:, 2:3] = (pc_xyzrgb[:, 2:3] - min_z) / np.maximum((max_z - min_z), 1e-3)
+    pc_xyzrgb = np.concatenate([pc_xyzrgb, ori_xyz], axis=-1)   # [points, 12]
+    temp[i] = pc_xyzrgb
+
+idx, points = farthest_point_sampling(temp)
+X = torch.tensor(temp, dtype=torch.float32)
+
+transformer = torch.load("J:\\COMProfile\\Downloads\\pk\\transformer_0411\\transformer_19.pth", map_location=torch.device('cpu'))
+transformer.eval()
+seg_net = torch.load("J:\\COMProfile\\Downloads\\pk\\transformer_0411\\seg_net_19.pth", map_location=torch.device('cpu'))
+seg_net.eval()
+global_features, points_features = transformer(X, points, idx, "cpu")
+predict_sem_labels = seg_net(global_features, points_features)
+predict_sem_labels = torch.squeeze(predict_sem_labels)
+predict_sem_labels = torch.argmax(predict_sem_labels, dim=-1)   #[batch, points]
+predict_sem_labels = predict_sem_labels.view(-1)
+predict_sem_labels = predict_sem_labels.numpy()
+
+
+Plot.draw_pc_semins(fin['coords'][:].reshape(-1, 3), predict_sem_labels, 13)
+"""
+
+
+"""
+# test_transformer: large blocks
+fin = h5py.File("./data/Area_5_hallway_1.h5", 'r')
+
+# [55, 4096, 3]   &    [55, 4096, 9]
+coords = fin['coords'][:]
+points = fin['points'][:]
+
+batch, num, features1 = coords.shape
+
+_, _, features2 = points.shape
+
+temp = np.zeros((batch, num, features1 + features2), dtype=np.float32)
+
+for i in range(batch):
+    pc_xyzrgb = np.concatenate([coords[i], points[i][:, 3:9]], axis=-1)
+    min_x = np.min(pc_xyzrgb[:, 0])
+    max_x = np.max(pc_xyzrgb[:, 0])
+    min_y = np.min(pc_xyzrgb[:, 1])
+    max_y = np.max(pc_xyzrgb[:, 1])
+    min_z = np.min(pc_xyzrgb[:, 2])
+    max_z = np.max(pc_xyzrgb[:, 2])
+
+    ori_xyz = copy.deepcopy(pc_xyzrgb[:, 0:3])  # reserved for final visualization
+    pc_xyzrgb[:, 0:1] = (pc_xyzrgb[:, 0:1] - min_x) / np.maximum((max_x - min_x), 1e-3)
+    pc_xyzrgb[:, 1:2] = (pc_xyzrgb[:, 1:2] - min_y) / np.maximum((max_y - min_y), 1e-3)
+    pc_xyzrgb[:, 2:3] = (pc_xyzrgb[:, 2:3] - min_z) / np.maximum((max_z - min_z), 1e-3)
+    pc_xyzrgb = np.concatenate([pc_xyzrgb, ori_xyz], axis=-1)   # [points, 12]
+    temp[i] = pc_xyzrgb
+
+
+transformer = torch.load("J:\\COMProfile\\Downloads\\pk\\transformer_0411\\transformer_19.pth", map_location=torch.device('cpu'))
+transformer.eval()
+seg_net = torch.load("J:\\COMProfile\\Downloads\\pk\\transformer_0411\\seg_net_19.pth", map_location=torch.device('cpu'))
+seg_net.eval()
+sem_labels = np.array([], dtype=np.int32)
+for i in range(0, batch, 6):
+    tempX = temp[i:i+6]
+    idx, points = farthest_point_sampling(tempX)
+    X = torch.tensor(tempX, dtype=torch.float32)
+    global_features, points_features = transformer(X, points, idx, "cpu")
+    predict_sem_labels = seg_net(global_features, points_features)
+    predict_sem_labels = torch.squeeze(predict_sem_labels)
+    predict_sem_labels = torch.argmax(predict_sem_labels, dim=-1)   # [6, points]
+    predict_sem_labels = predict_sem_labels.view(-1)                # [6 * points, ]
+    predict_sem_labels = predict_sem_labels.numpy()
+    sem_labels = np.concatenate((sem_labels, predict_sem_labels))
+
+Plot.draw_pc_semins(fin['coords'][:].reshape(-1, 3), sem_labels, 13)
 """
